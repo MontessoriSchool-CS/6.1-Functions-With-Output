f772ba9cce694453f2cb9c07b8df999a73bbbd20
CompSciMrF <17910442+geekyteacher@users.noreply.github.com>
2021-06-11 12:39:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task 1
+
+  # The program below uses functions.  Add comments to predict the output in the order that it has been coded here.
+
+def say_hi():
+  print("Why hello there!")
+
+def offer_drink():
+  print("Would you care for a spot of tea?")
+
+def offer_food():
+  print("Biscuit?")
+
+def say_bye():
+  print("Cheerio then.")
+
+
+offer_drink()
+say_hi()
+offer_food()
+
+# Task 2
+  # Rewrite the code to call the functions in the correct order so that the output makes sense chronologically.
+
+  # Call the correct function so that the program says goodbye to the user.
+
+
+# Task 3
+
+  # Define a new function that tells the user a joke (you decide on the funtion name and the joke).  Call it in a sensible place in the program.
